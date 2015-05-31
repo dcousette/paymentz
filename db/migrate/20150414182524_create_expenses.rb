8d96d39d9a78936c6c60cbe76f0f470bf6a66d6e
DeShawn <dcousette@gmail.com>
2015-05-31 17:36:23 +0000

    Deleted duplicate migration file with expenses table timestamps

--- db/migrate/20150414182524_create_expenses.rb
+++
@@ -1,9 +0,0 @@
-class CreateExpenses < ActiveRecord::Migration
-  def change
-    create_table :expenses do |t|
-      t.string :expense_name
-      t.float :expense_amount 
-      t.timestamps 
-    end
-  end
-end
