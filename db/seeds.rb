0ea0671b0b57d9bbe4ccd81e8d393f82170ae3c4
DeShawn Cousette <dcousette@gmail.com>
2015-11-29 01:05:20 -0500

    Add seed data. Correct error on payments#index template

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#users
+me =    User.create(username: 'dcousette', first_name: 'DeShawn', last_name: 'Cousette',
+            email_address: 'dcousette@gmail.com', password: 'dcousette')
+niven = User.create(username: 'niven', first_name: 'Niven', last_name: 'Cousette',
+            email_address: 'niven@gmail.com', password: 'niven')
+#expenses
+house =    Expense.create(name: 'Mortgage', amount: 1000.00 )
+gas =      Expense.create(name: 'Gas', amount: 200.00 )
+electric = Expense.create(name: 'Electric', amount: 100.00 )
+
+#payments
